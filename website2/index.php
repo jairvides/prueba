1db80ac21a51c174b6c987a2040fb28167d2902d
jairvides <79264513+jairvides@users.noreply.github.com>
2021-04-14 23:57:42 -0500

    Archivos
    Archivos php y css

---
+++ website2/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
+  <title>Jair Vides</title>
+  <meta name="autor" content="Jair Vides">
+  <meta name="descripcion" content="Sitio Web de Jair Vides">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+  <link rel="stylesheet" href="css/main.css">
+  <script type="text/javascript" src="https://use.typekit.net/nxs1qix.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+<body>
+  <div class="wrapper">
+    <div class="triangle-pattern"></div>
+    <div class="wrapper-inner">
+      <h1 class="name">Jair Enrique Vides Berdugo</h1>
+      <h2 class="residence">Algarrobo, Magdalena - Colombia</h2>
+      <ul class="social-links">
+        <li>
+          <a class="social-link-item social-link-item-twitter" target="_blank" href="https://twitter.com/JAIRVIDES">Twitter</a>
+        </li>
+        <li>
+          <a class="social-link-item social-link-item-linkedin" target="_blank" href="https://www.linkedin.com/in/jairvides">Linkedin</a>
+        </li>
+        <li>
+          <a class="social-link-item social-link-item-dribbble" target="_blank" href="https://dribbble.com/jairvides">Dribbble</a>
+        </li>
+        <li>
+          <a class="social-link-item social-link-item-github" target="_blank" href="https://github.com/jairvides">Github</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+ </body>
+</html>
